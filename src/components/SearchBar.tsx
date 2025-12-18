8ca34f27245a40fdf188c4583302687535881341
rahulgithubral <161911418+rahulgithubral@users.noreply.github.com>
2025-12-18 18:13:37 +0530

    Remove margin-bottom from SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -16,7 +16,7 @@ const SearchBar: React.FC = () => {
   }, [localQuery, debouncedSearch]);
 
   return (
-    <div className="relative mb-6">
+    <div className="relative">
       <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
         <svg
           className="h-5 w-5 text-gray-400"
